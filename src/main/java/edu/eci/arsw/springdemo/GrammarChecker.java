8b8531303979b7109a17baf28ede622897d0b620
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-02-18 21:16:03 -0500

    feat: prueba sin anotaciones solocon uso del aplication context

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -1,23 +1,15 @@
 package edu.eci.arsw.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Service
 public class GrammarChecker {
-
 
 	SpellChecker sc;
 
 	String x;
-        
-    @Autowired
-    public GrammarChecker(SpellChecker spellChecker) {
-        this.sc = spellChecker;
-    }
-	    
+
+	public GrammarChecker(SpellChecker spellChecker) {
+		this.sc = spellChecker;
+	}
+
 	public SpellChecker getSpellChecker() {
 		return sc;
 	}
@@ -26,17 +18,11 @@ public class GrammarChecker {
 		this.sc = sc;
 	}
 
-
-	public String check(String text){
-		
-		StringBuffer sb=new StringBuffer();
-		sb.append("Spell checking output:"+sc.checkSpell(text));
+	public String check(String text) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Spell checking output:" + sc.checkSpell(text));
 		sb.append("Plagiarism checking output: Not available yet");
-		
-		
 		return sb.toString();
-		
 	}
-	
-	
+
 }
--- src/main/java/edu/eci/arsw/springdemo/ui/Main.java
+++ src/main/java/edu/eci/arsw/springdemo/ui/Main.java
@@ -6,7 +6,6 @@
 package edu.eci.arsw.springdemo.ui;
 
 import edu.eci.arsw.springdemo.GrammarChecker;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -16,9 +15,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String a[]) {
-        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-        GrammarChecker gc = ac.getBean(GrammarChecker.class);
-        System.out.println(gc.check("la la la "));
+        try (ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+            GrammarChecker gc = ac.getBean(GrammarChecker.class);
+            System.out.println(gc.check("la la la "));
+        }
+        
     }
 
 }
